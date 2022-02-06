c8531eae44171e82e631f8237e1ce94d304de009
Sina <Moradix96@gmail.com>
2022-02-06 20:44:16 +0330

    Add a webview to MainActivity and post url to it.

--- app/src/main/java/ir/samiantec/androidpostdatatowebview/MainActivity.java
+++ app/src/main/java/ir/samiantec/androidpostdatatowebview/MainActivity.java
@@ -1,8 +1,12 @@
 package ir.samiantec.androidpostdatatowebview;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final WebView webView = findViewById(R.id.webView);
+
+        final String url = "https://httpbin.org/post"; // Echos every post request
+        try {
+            final String postData = "key1=" + URLEncoder.encode("value1", "UTF-8") + "&key2=" + URLEncoder.encode("value2", "UTF-8");
+            webView.postUrl(url, postData.getBytes());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 }
